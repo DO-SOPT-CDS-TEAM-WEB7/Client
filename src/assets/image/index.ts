77f623d79a078790d1dc22c11277b1698535d85b
se0jinYoon <tjwls1999@gmail.com>
2023-11-22 03:34:48 +0900

    feat: Nav바 로고, 검색리스트 구현

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -1,3 +1,4 @@
 // 명명규칙은 xxxxImg로 통일
+export { default as SearchImg } from './icn_search.png';
 export { default as MainImg } from './img_main.png';
-export { default as LogoImg } from './logo.svg';
+export { default as LogoIcon } from './logo.svg';
--- src/components/ticketList/Nav.tsx
+++ src/components/ticketList/Nav.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { LogoImg } from '../../assets/image';
+import { LogoIcon, SearchImg } from '../../assets/image';
 
 const Nav = () => {
-  return <St.NavContainer></St.NavContainer>;
+  return (
+    <St.NavContainer>
+      <St.SearchBarContainer>
+        <img src={LogoIcon} alt="로고" />
+        <St.SearchBarUl>
+          <St.SearchBarLi>도움말</St.SearchBarLi>
+          <St.SearchBarLi>위시리스트</St.SearchBarLi>
+          <St.SearchBarLi>로그인</St.SearchBarLi>
+          <St.SearchImg src={SearchImg} alt="검색 아이콘" />
+        </St.SearchBarUl>
+      </St.SearchBarContainer>
+    </St.NavContainer>
+  );
 };
 
 export default Nav;
@@ -12,5 +24,27 @@ export default Nav;
 const St = {
   NavContainer: styled.nav`
     padding: 3.6rem 16rem;
+  `,
+  SearchBarContainer: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  `,
+  SearchBarUl: styled.ul`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1.05rem 0;
+  `,
+  SearchBarLi: styled.li`
+    margin-right: 2.4rem;
+    cursor: pointer;
+    padding: 0.4rem;
+    ${({ theme }) => theme.fonts.body06};
+  `,
+  SearchImg: styled.img`
+    cursor: pointer;
+    width: 3.6rem;
+    height: 3.6rem;
   `,
 };

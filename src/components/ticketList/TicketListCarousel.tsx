a1e1db3a85ab30dc73993487f7045a617638ea4d
se0jinYoon <tjwls1999@gmail.com>
2023-11-24 17:13:35 +0900

    feat: 항공권 리스트 컴포넌트, 더미데이터 복구

---
+++ src/components/ticketList/TicketListCarousel.tsx
@@ -0,0 +1,151 @@
+import styled from 'styled-components';
+
+import { HeartImg, HotelImg, StarImg } from '../../assets/image';
+
+interface TicketListCarouselProps {
+  hotelName: string;
+  hotelRoom: string;
+  hotelBed: string;
+}
+
+const TicketListCarousel = (props: TicketListCarouselProps) => {
+  const { hotelName, hotelRoom, hotelBed } = props;
+  return (
+    <ElementContainer>
+      <Heart src={HeartImg} alt="좋아요" />
+      <ElementImg src={HotelImg} alt="이미지" />
+      <DescriptionWrapper>
+        <HotelDescription>
+          <HotelName>{hotelName}</HotelName>
+          <HotelInfoContainer>
+            <HotelRoomInfo>{hotelRoom}</HotelRoomInfo>
+            <HotelBedInfo>{hotelBed}</HotelBedInfo>
+          </HotelInfoContainer>
+        </HotelDescription>
+        <HotelPriceStarContainer>
+          <HotelStarContainer>
+            <HotelStarImg src={StarImg} alt="별점" />
+            <HotelStarRate>4.7</HotelStarRate>
+            <HotelStarNum>(173)</HotelStarNum>
+          </HotelStarContainer>
+          <HotelPriceContainer>
+            <HotelPrice>75,113원</HotelPrice>
+            <HotelPerDay>/박</HotelPerDay>
+          </HotelPriceContainer>
+        </HotelPriceStarContainer>
+      </DescriptionWrapper>
+    </ElementContainer>
+  );
+};
+
+export default TicketListCarousel;
+
+const ElementContainer = styled.article`
+  position: relative;
+  border-radius: 1.6rem;
+  width: 25rem;
+  height: 30.8rem;
+`;
+
+const ElementImg = styled.img`
+  border-radius: 0.1rem 0.1rem 0 0;
+  width: 100%;
+  height: 20.2rem;
+`;
+
+const DescriptionWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border: 0;
+  border-right: 1px solid ${({ theme }) => theme.colors.skscanGrey200};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.skscanGrey200};
+  border-left: 1px solid ${({ theme }) => theme.colors.skscanGrey200};
+  border-radius: 0 0 1.6rem 1.6rem;
+  padding: 2rem;
+  height: 10.6rem;
+`;
+
+const HotelDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const HotelName = styled.p`
+  margin-bottom: 0.5rem;
+  ${({ theme }) => theme.fonts.body05};
+`;
+
+const HotelInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 0.5rem;
+  ${({ theme }) => theme.fonts.caption02};
+`;
+
+const HotelRoomInfo = styled.span`
+  margin-bottom: 0.5rem;
+  color: ${({ theme }) => theme.colors.skscanGrey700};
+`;
+const HotelBedInfo = styled.span`
+  color: ${({ theme }) => theme.colors.skscanGrey700};
+`;
+
+const HotelPriceStarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const HotelStarContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 1.9rem;
+  ${({ theme }) => theme.fonts.caption02};
+`;
+
+const HotelStarImg = styled.img`
+  width: 2.4rem;
+  height: 2.4rem;
+`;
+
+const HotelStarRate = styled.span`
+  padding: 0.35rem 0;
+  color: ${({ theme }) => theme.colors.skscanBk};
+`;
+
+const HotelStarNum = styled.span`
+  padding: 0.35rem 0 0.35rem 0.2rem;
+  color: ${({ theme }) => theme.colors.skscanGrey700};
+`;
+
+const HotelPriceContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  margin-top: 0.5rem;
+`;
+
+const HotelPrice = styled.span`
+  margin-right: 0.2rem;
+  color: ${({ theme }) => theme.colors.skscanBk};
+  ${({ theme }) => theme.fonts.caption01};
+`;
+
+const HotelPerDay = styled.span`
+  color: ${({ theme }) => theme.colors.skscanGrey700};
+  ${({ theme }) => theme.fonts.caption02};
+`;
+
+const Heart = styled.img`
+  position: absolute;
+  top: 0.8rem;
+  right: 1.2rem;
+  cursor: pointer;
+  width: 4.8rem;
+  height: 4.8rem;
+`;
--- src/components/ticketList/TicketListTest.tsx
+++ src/components/ticketList/TicketListTest.tsx
@@ -1,5 +1,58 @@
+// eslint-disable-next-line simple-import-sort/imports
+import TicketListCarousel from './TicketListCarousel';
+import Carousel from '../common/Carousel';
+
+const DUMMY = [
+  {
+    hotelName: '삿포로의 호스텔',
+    hotelRoom: '더블룸 (전용 욕실)',
+    hotelBed: '침대 1개',
+  },
+  {
+    hotelName: '삿포로의 호스텔',
+    hotelRoom: '더블룸 (전용 욕실)',
+    hotelBed: '침대 1개',
+  },
+  {
+    hotelName: '삿포로의 호스텔',
+    hotelRoom: '더블룸 (전용 욕실)',
+    hotelBed: '침대 1개',
+  },
+  {
+    hotelName: '삿포로의 호스텔',
+    hotelRoom: '더블룸 (전용 욕실)',
+    hotelBed: '침대 1개',
+  },
+  {
+    hotelName: '삿포로의 호스텔',
+    hotelRoom: '더블룸 (전용 욕실)',
+    hotelBed: '침대 1개',
+  },
+  {
+    hotelName: '삿포로의 호스텔',
+    hotelRoom: '더블룸 (전용 욕실)',
+    hotelBed: '침대 1개',
+  },
+  {
+    hotelName: '삿포로의 호스텔',
+    hotelRoom: '더블룸 (전용 욕실)',
+    hotelBed: '침대 1개',
+  },
+  {
+    hotelName: '삿포로의 호스텔',
+    hotelRoom: '더블룸 (전용 욕실)',
+    hotelBed: '침대 1개',
+  },
+];
+
 const TicketListTest = () => {
-  return <div>TicketListTest</div>;
+  return (
+    <Carousel dataLength={DUMMY.length}>
+      {DUMMY.map((item, idx) => (
+        <TicketListCarousel key={idx} hotelName={item.hotelName} hotelRoom={item.hotelRoom} hotelBed={item.hotelBed} />
+      ))}
+    </Carousel>
+  );
 };
 
 export default TicketListTest;

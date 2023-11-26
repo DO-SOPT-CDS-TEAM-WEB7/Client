f74c4c1c5d7d695a2920bf941860203c8b198d5c
se0jinYoon <tjwls1999@gmail.com>
2023-11-27 03:02:21 +0900

    chore: router 테스트 코드 삭제

--- src/components/common/Router.tsx
+++ src/components/common/Router.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Main from '../../pages/Main';
-import HotelItem from '../ticketList/HotelCarousel';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/ticketList" element={<HotelItem />} />
       </Routes>
     </BrowserRouter>
   );

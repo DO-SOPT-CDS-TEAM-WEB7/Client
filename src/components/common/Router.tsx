45573a9db66c0da712d73ee14a087c5dad3dee3b
se0jinYoon <tjwls1999@gmail.com>
2023-11-27 03:01:11 +0900

    feat: 기본 파일 생성

--- src/components/common/Router.tsx
+++ src/components/common/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Main from '../../pages/Main';
+import Ticket from '../ticketList/Ticket';
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/ticketList" element={<Ticket />} />
       </Routes>
     </BrowserRouter>
   );

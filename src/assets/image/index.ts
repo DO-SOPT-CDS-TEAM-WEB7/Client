c5b48e4da81bc819070811b07b2a7f676eeb6bb8
aazkgh <gg25hp17@naver.com>
2023-11-27 17:00:49 +0900

    feat/#26: 인사이트 컴포넌트 구현 완료

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -1,2 +1,4 @@
 // 명명규칙은 xxxxImg로 통일
-export { default as MainImg } from './img_main.png';
+export { default as RightArticleImg2 } from './image.png';
+export { default as RightArticleImg1 } from './image-1.png';
+export { default as LeftArticleImg } from './Rectangle 34624743.png';

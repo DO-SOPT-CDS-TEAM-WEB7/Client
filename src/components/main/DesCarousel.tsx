95c64fdb214b7251f2b54bbf8ec7dfc76d6a3bfb
aazkgh <gg25hp17@naver.com>
2023-11-28 02:29:04 +0900

    feat/#28: 캐러셀 이용해서 공통 컴포넌트 구현 완료

---
+++ src/components/main/DesCarousel.tsx
@@ -0,0 +1,16 @@
+import DES_ITEM_DATA from '../../data/DesItemData';
+import Carousel from '../common/Carousel';
+// eslint-disable-next-line import/order
+import DesItem, { DesItemProps } from './DesItem';
+
+const DesCarousel = () => {
+  return (
+    <Carousel dataLength={DES_ITEM_DATA.length}>
+      {DES_ITEM_DATA.map((item: DesItemProps, idx: number) => (
+        <DesItem key={idx} desName={item.desName} desImg={item.desImg} />
+      ))}
+    </Carousel>
+  );
+};
+
+export default DesCarousel;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,13 +1,27 @@
+import styled from 'styled-components';
+
 import Header from '../components/main/Header';
 import QnA from '../components/main/QnA';
+import RecommendDestination from '../components/main/RecommendDestination';
 
 const Main = () => {
   return (
     <>
       <Header />
-      <QnA />;
+      <Container>
+        <RecommendDestination />
+        <QnA />;
+      </Container>
     </>
-   );
+  );
 };
 
 export default Main;
+
+export const Container = styled.main`
+  display: flex;
+  position: relative;
+  top: 14.6rem;
+  flex-direction: column;
+  gap: 10rem;
+`;

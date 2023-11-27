8a9f2d21d1fccf853814948235b488b0d4ea8a67
simeunseo <rkdcjfehowl@naver.com>
2023-11-27 17:26:58 +0900

    style: 콘솔로그 삭제

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 import { ChevronDownIcon } from '../../assets/icon';
 
 const Footer = () => {
-  console.log(ChevronDownIcon);
   return (
     <FooterContainer>
       <LeftSide>

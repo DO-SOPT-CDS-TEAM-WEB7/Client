1e65abadbede449b3f5eb203d870e686baba7a6b
se0jinYoon <tjwls1999@gmail.com>
2023-11-29 18:03:53 +0900

    design: 캐러셀 아이템 배경색 추가

--- src/components/ticketList/AdvImg.tsx
+++ src/components/ticketList/AdvImg.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+
 import { AdImg } from '../../assets/image';
 
 const AdvImg = () => {
-  return (
-    <AdImgContainer src={AdImg} alt="광고 이미지" />
-  )
-}
+  return <AdImgContainer src={AdImg} alt="광고 이미지" />;
+};
 
 export default AdvImg;
 
 const AdImgContainer = styled.img`
-    width: 100%;
-    height: 9.2rem;
-    margin-top: 3.6rem;
-    margin-bottom: 4.8rem;
-`
+  margin-top: 3.6rem;
+  margin-bottom: 4.8rem;
+  width: 100%;
+  height: 9.2rem;
+`;

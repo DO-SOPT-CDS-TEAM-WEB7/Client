da80377af6d4db1eca9a1007293d6a93b4e3fd94
simeunseo <rkdcjfehowl@naver.com>
2023-11-29 19:34:17 +0900

    feat: type 추가

--- src/apis/getApiTest.ts
+++ src/apis/getApiTest.ts
@@ -1,5 +1,7 @@
+import { ApiTestResponse } from '../types/apiTest';
+// eslint-disable-next-line import/order
 import { client } from './client';
 
 export const getApiTest = () => {
-  return client.get(`/air/min-prices`);
+  return client.get<ApiTestResponse>(`/air/min-prices`);
 };
--- src/pages/ApiTest.tsx
+++ src/pages/ApiTest.tsx
@@ -4,6 +4,9 @@ import { getApiTest } from '../apis/getApiTest';
 
 const ApiTest = () => {
   const getMinPriceList = async () => {
+    // 여기서 api 호출 함수 불러서 async/await으로 처리!
+    // {data : {data}} 는 구조분해할당! 처음에 data만 불러오게 되면 상태코드, message, data가
+    // 다 담겨있기 때문에 다시 한 번 구조분해할당으로 data에 접근하는 것!
     try {
       const {
         data: { data },

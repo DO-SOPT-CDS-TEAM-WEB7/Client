eefcd53a4cf32c2d7e618039d7441b079a104740
se0jinYoon <tjwls1999@gmail.com>
2023-12-01 04:54:06 +0900

    feat: 서버 api 연결, 더미데이터 삭제

--- src/apis/getTicketList.ts
+++ src/apis/getTicketList.ts
@@ -2,6 +2,6 @@ import { TicketListRequestBody, TicketListResponse } from '../types/ticketList';
 // eslint-disable-next-line import/order
 import { client } from './client';
 
-export const getTicketList = (body: TicketListRequestBody) => {
-  return client.get<TicketListResponse>(`air/reservations`, { data: body });
+export const getTicketList = (queryParams: TicketListRequestBody) => {
+  return client.get<TicketListResponse>(`air/reservations`, { params: queryParams });
 };

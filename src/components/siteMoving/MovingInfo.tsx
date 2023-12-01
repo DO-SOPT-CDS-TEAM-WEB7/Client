9c71b4cfcb47a26a163ae759777bd30fa46dc750
simeunseo <rkdcjfehowl@naver.com>
2023-12-01 20:39:12 +0900

    feat: 이동 정보 일러스트 구현

--- src/components/siteMoving/MovingInfo.tsx
+++ src/components/siteMoving/MovingInfo.tsx
@@ -1,12 +1,21 @@
+import styled from 'styled-components';
+
+import { LogoBigIcon } from '../../assets/icon';
+import { MovingAirplane } from '../../assets/image';
+import AGENCY_NO_BG_IMG from '../../data/AgencyNoBgData';
+
 const MovingInfo = () => {
   return (
     <MovingInfoContainer>
       <Title>예약 사이트로 이동 중 입니다.</Title>
+      <MovingIllust>
+        <StyledImg src={LogoBigIcon} />
+        <StyledImg src={MovingAirplane} />
+        <StyledImg src={AGENCY_NO_BG_IMG['노랑풍선']} />
+      </MovingIllust>
     </MovingInfoContainer>
   );
 };
-
-import styled from 'styled-components';
 
 export default MovingInfo;
 
@@ -22,4 +31,21 @@ const MovingInfoContainer = styled.div`
 const Title = styled.h1`
   color: ${({ theme }) => theme.colors.skscanWt};
   ${({ theme }) => theme.fonts.heading04}
+`;
+
+const MovingIllust = styled.article`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 5.4rem;
+  border: 1px solid ${({ theme }) => theme.colors.skscanWt};
+  border-radius: 1rem;
+  background-color: rgb(255 255 255 / 40%);
+  padding: 3.8rem 2.3rem;
+  backdrop-filter: blur(3px);
+`;
+
+const StyledImg = styled.img`
+  width: min-content;
+  height: min-content;
 `;
